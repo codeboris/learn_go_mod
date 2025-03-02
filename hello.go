9b6adba89778df0148ee7c4d8e026e3f3c96dd99
codeboris <77102100+codeboris@users.noreply.github.com>
2025-03-02 13:11:47 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,15 +2,6 @@ package hello
 
 import "fmt"
 
-var Version = "v1.0.0"
-
 func Hello() {
-    switch Version {
-    case "v1.0.0":
-        fmt.Println("Hello, v1.0.0")
-    case "v1.1.0":
-        fmt.Println("Hello, v1.1.0")
-    default:
-        fmt.Println("Unknown version")
-    }
+    fmt.Println("Hello, v1.0.0")
 }
