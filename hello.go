24436e7a8ae3494da4c4ae0bec0c056d829dc675
codeboris <77102100+codeboris@users.noreply.github.com>
2025-03-02 04:07:56 +0300

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,16 @@
+package hello
+
+import "fmt"
+
+var Version string
+
+func Hello() {
+    switch Version {
+    case "v1.0.0":
+        fmt.Println("Hello, v1.0.0")
+    case "v1.1.0":
+        fmt.Println("Hello, v1.1.0")
+    default:
+        fmt.Println("Unknown version")
+    }
+}
